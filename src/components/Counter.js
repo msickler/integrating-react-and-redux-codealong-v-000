5dd2a56cac2162526604b91135d964bac0db70eb
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-03 22:54:02 +0000

    automatically backed up by learn

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
-  export default (props) => {
 
-    const handleOnClick = () => {
-      props.store.dispatch({ type: 'INCREASE_COUNT' });
-    }
+export default (props) => {
 
-    return (
-      <div>
-        <button onClick={handleOnClick}>
-          Click Me
-        </button>
-        <div>{props.store.getState().count}</div>
-      </div>
-    )
-  };
+ const handleOnClick = () => {
+  props.store.dispatch({ type: 'INCREASE_COUNT' });
+  }
+
+  return (
+    <div>
+      <button onClick={handleOnClick}>
+        Click Me
+      </button>
+    <div>{props.store.getState() ? props.store.getState().count : 0}</div>
+  </div>
+  )
+};
