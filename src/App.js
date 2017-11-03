6b397fef97580216df689ac0a8d2cfa5d87d8d4a
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-03 22:45:45 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-        Counter Component here.
+        <Counter store={this.props.store} />
       </div>
     );
   }
