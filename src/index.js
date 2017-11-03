33677fe36339fe2a341138d3bd041bbb8d6df825
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-03 22:52:55 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
- import ReactDOM from 'react-dom';
- import App from './App';
- import changeCount from './reducers/changeCount';
- import createStore from './createStore';
-
- const store = createStore(changeCount);
-
- export function render() {
-   ReactDOM.render(
-     <App store={store} />,
-     document.getElementById('root')
-   );
- };
-
- render();
+import ReactDOM from 'react-dom';
+import App from './App';
+import changeCount from './reducers/changeCount';
+import createStore from './createStore';
+ 
+const store = createStore(changeCount);
+ 
+export function render() {
+  ReactDOM.render(
+    <App store={store} />,
+    document.getElementById('root')
+  );
+};
+ 
+store.dispatch({ type: '@@INIT' });
+ 
+// remove render() here
